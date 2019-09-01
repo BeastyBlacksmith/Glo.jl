9e0600c4ec741ebf3708c1f07226d4fe15647651
Simon Christ <SimonChrist@gmx.de>
2019-09-01 14:40:35 +0200

    identified naming issue

--- src/Glo.jl
+++ src/Glo.jl
@@ -5,6 +5,7 @@ using MacroTools: striplines
 using JSON
 using HTTP
 # Kindly adopted form Lyndon White (https://github.com/oxinabox/RESTful.jl/blob/master/src/proto.ipynb)
+# TODO: make this accept authentifiation, create distinguishable methods for apis that target the same URI or prepend the REST-method to method names
 function declare_api(root, method, endpoint, param_names)
     function_name = Symbol(join(split(endpoint, "/"; keepempty=false), "_"))
     param_sig = Expr(:parameters,Expr.(:kw, Symbol.(param_names), :nothing)...)
--- src/declarations.jl
+++ src/declarations.jl
@@ -1,1 +1,2 @@
 @eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "GET", "/boards", ["fields", "archived", "page", "per_page", "sort"]))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "POST", "/boards", []))
