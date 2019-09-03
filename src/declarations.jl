2ae6580873e28355fa462d0af462321607d3b9f7
Simon Christ <SimonChrist@gmx.de>
2019-09-03 11:09:32 +0200

    add columns, cards and users API

--- src/declarations.jl
+++ src/declarations.jl
@@ -14,5 +14,20 @@ end
 @eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "POST", "/boards/{board_id}", []))
 @eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "DELETE", "/boards/{board_id}", []))
 ## Column API
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "POST", "/boards/{board_id}/columns", []))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "POST", "/boards/{board_id}/columns/batch", []))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "POST", "/boards/{board_id}/columns/{column_id}", []))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "DELETE", "/boards/{board_id}/columns/{column_id}", []))
 ## Card API
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "GET", "/boards/{board_id}/cards", ["fields", "archived", "page", "per_page", "sort"]))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "POST", "/boards/{board_id}/cards", []))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "POST", "/boards/{board_id}/cards/batch", []))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "GET", "/boards/{board_id}/cards/{card_id}", ["fields"]))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "POST", "/boards/{board_id}/cards/{card_id}", []))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "DELETE", "/boards/{board_id}/cards/{card_id}", []))
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "GET", "/boards/{board_id}/columns/{column_id}/cards", ["fields", "archived", "page", "per_page", "sort"]))
+## Lables API
+## Attachments API
+## Comments API
 ## User API
+@eval $(declare_api("https://gloapi.gitkraken.com/v1/glo", "GET", "/user", ["fields"]))
